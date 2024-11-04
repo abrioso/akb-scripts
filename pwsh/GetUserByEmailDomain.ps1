29ef022b03858b1b122247d73161557f158cba39
Andre Kakoo Brioso <akbrioso@iseg.ulisboa.pt>
2024-11-04 13:06:55 +0000

    Enhance GetUserByEmailDomain script to accept user-defined email domain and export results to CSV

--- pwsh/GetUserByEmailDomain.ps1
+++ pwsh/GetUserByEmailDomain.ps1
@@ -8,17 +8,25 @@ if (-not (Get-Module -ListAvailable -Name Microsoft.Graph)) {
 # Import the Microsoft.Graph module
 Import-Module Microsoft.Graph
 
+# Read the domain from the user
+$domain = Read-Host -Prompt 'Enter the domain to search for users'
+$date = Get-Date -Format "yyyyMMdd"
+
 # Connect to Microsoft Graph
 Connect-MgGraph -Scopes "User.Read.All"
 
 # Get all users
 $users = Get-MgUser -All
 
-# Filter users with '@local' suffix in their email
-$localUsers = $users | Where-Object { $_.Mail -like '*@local' }
+# Filter users with the specified domain in their email
+$localUsers = $users | Where-Object { $_.Mail -like "*$domain" }
 
 # Display the users
 $localUsers | Select-Object UserPrincipalName, DisplayName, Mail, AccountEnabled
+
+# Export the users to a CSV file and properties to a cxv file
+#$localUsers | Export-Csv -Path "users_$($domain -replace '@', '')_$date.csv" -NoTypeInformation
+$localUsers | Select-Object UserPrincipalName, DisplayName, Mail, AccountEnabled | Export-Csv -Path "users_$($domain -replace '@', '')_$date.cxv" -NoTypeInformation
 
 # Disconnect from Microsoft Graph
 Disconnect-MgGraph
